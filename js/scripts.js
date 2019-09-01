2398132499da2e672e4675670cec25c74a36c468
Nkurumwa <nkurumwa51@gmail.com>
2019-09-01 13:49:32 +0300

    validates pizza cost

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,21 @@ function Pizza(crust, size) {
     this.toppings = [];
     this.price = 0;
   }
+  Pizza.prototype.cost = function() {
+    var price = 0;
+    if (this.size === "Small") {
+      price += 7;
+    } else if (this.size === "Medium") {
+      price += 9;
+    } else if (this.size === "Large") {
+      price += 11;
+    } else {
+      price+= 16;
+    }
+  
+    for (var i = 0; i < this.toppings.length; i++) {
+      price += 1;
+    }
+  
+    this.price = price;
+  }
